849f49ea7a0cc94038021b2859b18c8f26244f54
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-05-24 15:43:13 -0700

    Added blacklisted words

--- Cog/developer.py
+++ Cog/developer.py
@@ -29,23 +29,28 @@ class Developer(commands.Cog, name="Developer"):
             embed = discord.Embed(title="Slow down there cowboy!", description="You can only have 3 active projects at a time!", color=discord.Color.red())
             await ctx.send(embed=embed)
         else:
-            if category is None:
-                await ctx.guild.create_category(username)
-            category = get(ctx.guild.categories, name=username)
-            channel = await guild.create_text_channel(name, overwrites=overwrites, category=category)
-            embed = discord.Embed(title="Success!", description="Your project is ready to go!", color=discord.Color.blue())
-            print(username,"has created a new project with the name",name)
-            await ctx.send(embed=embed)
-            username = ctx.message.author.name
-            embed = discord.Embed(title="Welcome!", description=f"Hey {username}, welcome to your new project! Now\n"
-                                                                f"that you're ready to go, lets find some team\n"
-                                                                f"members and get this thing rolling!\n"
-                                                                f"\n**How to start:** \n"
-                                                                f"\t- Get the word out! Type `-search` to begin.\n"
-                                                                f"\t- Invite people! Type `-inv [name]` to add them.\n"
-                                                                f"\t- Start planning! Every great idea needs a plan.\n"
-                                                                ,color=discord.Color.blue())
-            await channel.send(embed=embed)
+            blacklist_words = ["fuck", "shit", "cunt", "nigger", "niger", "niqqa", "bitch", "pussy", "penis", "dick", "boobs", "tits", "vagina", "ass", "retard"]
+            if name in blacklist_words:
+                embed = discord.Embed(title="Woah there!", description="Your project contained profanity!\n\n**Reminder:**\n- No projects with vulgar names\n- Projects must be child friendly", color=discord.Color.red())
+                await ctx.send(embed=embed)
+            else:
+                if category is None:
+                    await ctx.guild.create_category(username)
+                category = get(ctx.guild.categories, name=username)
+                channel = await guild.create_text_channel(name, overwrites=overwrites, category=category)
+                embed = discord.Embed(title="Success!", description="Your project is ready to go!", color=discord.Color.blue())
+                print(username,"has created a new project with the name",name)
+                await ctx.send(embed=embed)
+                username = ctx.message.author.name
+                embed = discord.Embed(title="Welcome!", description=f"Hey {username}, welcome to your new project! Now\n"
+                                                                    f"that you're ready to go, lets find some team\n"
+                                                                    f"members and get this thing rolling!\n"
+                                                                    f"\n**How to start:** \n"
+                                                                    f"\t- Get the word out! Type `-search` to begin.\n"
+                                                                    f"\t- Invite people! Type `-inv [name]` to add them.\n"
+                                                                    f"\t- Start planning! Every great idea needs a plan.\n"
+                                                                    ,color=discord.Color.blue())
+                await channel.send(embed=embed)
 
     @commands.has_role("Developer")
     @commands.command()
@@ -112,7 +117,6 @@ class Developer(commands.Cog, name="Developer"):
             if found is False:
                 embed = discord.Embed(title="Whoops!", description="You don't have a project with that name!", color=discord.Color.red())
                 await ctx.send(embed=embed)
-
-
+        
 def setup(bot):
     bot.add_cog(Developer(bot))
