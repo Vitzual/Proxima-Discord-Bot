92dbe8a25f0ccb2168e60507e465aee0e9063bdd
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-05-26 23:36:54 -0700

    Added confirmation prompt to leave command

--- Cog/developer.py
+++ Cog/developer.py
@@ -453,6 +453,8 @@ class Developer(commands.Cog, name="Developer"):
         ###########################################
         # You can change these for your own project
         COMMAND_ENABLED = True
+        CONFIRMATION_WORD = "confirm"
+        CONFIRMATION_PROMPT = True
         ###########################################
 
         if COMMAND_ENABLED is False:
@@ -464,8 +466,22 @@ class Developer(commands.Cog, name="Developer"):
         if "Projects" not in project.category.name:
             return
 
+        def check(m):
+            return m.author == ctx.author
+
         for scan in project.members:
             if scan.id == ctx.author.id:
+                if CONFIRMATION_PROMPT is True:
+                    embed = discord.Embed(title="Confirm",
+                                          description=f"Please type `{CONFIRMATION_WORD}` to leave the project",
+                                          color=discord.Color.blue())
+                    await ctx.send(embed=embed)
+                    msg = await self.bot.wait_for('message', timeout=60.0, check=check)
+                    if msg.content.lower() != CONFIRMATION_WORD:
+                        embed = discord.Embed(title="Confirmation failed!",
+                                              description="You did not confirm correctly!", color=discord.Color.red())
+                        await ctx.send(embed=embed)
+                        return
                 overwrite = discord.PermissionOverwrite()
                 overwrite.send_messages = False
                 overwrite.read_messages = False
@@ -477,10 +493,15 @@ class Developer(commands.Cog, name="Developer"):
                     embed = discord.Embed(title="Project left!", description=f"You left the project {project}!",
                                           color=discord.Color.red())
                     msg = await ctx.author.send(embed=embed)
+                    return
                 else:
                     embed = discord.Embed(title="Project left!", description=f"You left the project {project}!",
                                           color=discord.Color.blue())
                     msg = await ctx.send(embed=embed)
+                    return
+        embed = discord.Embed(title="Whoops!", description=f"You're not a part of that project",
+                              color=discord.Color.red())
+        msg = await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Developer(bot))
