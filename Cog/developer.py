aa0fe836eabb4a42e8aa94972224c7250d8490dd
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-05-27 22:33:51 -0700

    Added database integration for invite system

--- Cog/developer.py
+++ Cog/developer.py
@@ -372,7 +372,6 @@ class Developer(commands.Cog, name="Developer"):
 
     @commands.has_role("Verified Developer")
     @commands.command(aliases=["inv"])
-    # @commands.cooldown(1, 60, commands.BucketType.user)
     async def invite(self, ctx, member: discord.Member):
         """Invite people to your project"""
 
@@ -381,41 +380,70 @@ class Developer(commands.Cog, name="Developer"):
         ###########################################
         # You can change these for your own project
         COMMAND_ENABLED = True
-        SELF_CHECK = True
-        ROLE_CHECK = True
-        ALREADY_JOINED_CHECK = True
-        ROLE_NAMES = ["Verified Developer",
-                      "Verified Designer",
-                      "Verified Composer",
-                      "Verified Tester"]
+        SELF_CHECK = False
+        ROLE_CHECK = False
+        ALREADY_JOINED_CHECK = False
+        ROLE_NAMES = ["Verified Developer", 
+                    "Verified Designer", 
+                    "Verified Composer", 
+                    "Verified Tester"]
         OPT_OUT_LIST_CHECK = True
-        DATABASE_FILE_NAME = "opt-out"
-        DATABASE_EXTENSION = ".json"  # Dont change!
+        DATABASE_OPT_OUT = "opt-out"
+        DATABASE_FILE_NAME = "project-list"
+        DATABASE_EXTENSION = ".json" # Dont change!
         WRITE_PERMISSION = True
         SERVER_BOT_CHECK = True
         ###########################################
         # When INVITE_SYSTEM is set to false, will
         # automatically add the user to the channel
         # without the user needing to accept
-        INVITE_SYSTEM = True
+        INVITE_SYSTEM = True 
         ###########################################
 
         if COMMAND_ENABLED is False:
-            embed = discord.Embed(title="Command disabled", description="Looks like this command is disabled!",
-                                  color=discord.Color.red())
+            embed = discord.Embed(title="Command disabled", description="Looks like this command is disabled!", color=discord.Color.red())
             await ctx.send(embed=embed)
             return
 
+        if DATABASE_EXTENSION is not ".json":
+            embed = discord.Embed(title="WARNING", description=f"**Invalid database extension set!**\nIt looks like this value was changed.\n\n**Error:** Database must use .json files!\n*Revert this change, and then reload the module.*", color=discord.Color.red())
+            await ctx.send(embed=embed)
+            return
+
+        database = (DATABASE_FILE_NAME + DATABASE_EXTENSION)
+        valid = False
+        guild = ctx.guild
+
+        try:
+            with open(database) as f:
+                data = json.load(f)
+        except Exception as ex:
+            embed = discord.Embed(title="Database Error!",
+                                description=f"**Error:** {ex}",
+                                color=discord.Color.red())
+            await ctx.send(embed=embed)
+        else:
+            for scan in data:
+                if scan["user_id"] == ctx.author.id:
+                    valid = True
+                    category = get(guild.categories, id=scan["project_owned"])
+                    perms = category.channels
+                    use_channel = perms[1]
+                    members = scan["members"]
+                    description = scan["description"]
+
+        if valid is False:
+            embed = discord.Embed(title="Whoops!", description="You need to own a project first.", color=discord.Color.red())
+            await ctx.send(embed=embed)
+            return 
+
         if SERVER_BOT_CHECK is True and member.bot is True:
-            embed = discord.Embed(title="I'm flattered <3", description="You invited me to your project? *blushes*",
-                                  color=discord.Color.blue())
+            embed = discord.Embed(title="I'm flattered <3", description="You invited me to your project? *blushes*", color=discord.Color.blue())
             await ctx.send(embed=embed)
             return
 
         if SELF_CHECK is True and ctx.author == member:
-            embed = discord.Embed(title="Uh, *ding dong*, anyone home?",
-                                  description="You can't invite yourself to your own project",
-                                  color=discord.Color.red())
+            embed = discord.Embed(title="Uh, *ding dong*, anyone home?", description="You can't invite yourself to your own project", color=discord.Color.red())
             await ctx.send(embed=embed)
             return
 
@@ -427,107 +455,58 @@ class Developer(commands.Cog, name="Developer"):
                 if scan.name in ROLE_NAMES:
                     found = True
             if found is False:
-                embed = discord.Embed(title="Whoops!", description="You can only invite verified roles!",
-                                      color=discord.Color.red())
+                embed = discord.Embed(title="Whoops!", description="You can only invite verified roles!", color=discord.Color.red())
                 await ctx.send(embed=embed)
                 return
 
         if OPT_OUT_LIST_CHECK is True:
-            database = (DATABASE_FILE_NAME + DATABASE_EXTENSION)
-            with open(database) as f:
+            db = (DATABASE_OPT_OUT+DATABASE_EXTENSION)
+            with open(db) as f:
                 data = json.load(f)
             for user in data:
                 if user["user_id"] == member.id:
-                    embed = discord.Embed(title="Whoops!",
-                                          description="**That member has invites disabled!**\nIf you believe this is a mistake, let them know!",
-                                          color=discord.Color.red())
+                    embed = discord.Embed(title="Whoops!", description="**That member has invites disabled!**\nIf you believe this is a mistake, let them know!", color=discord.Color.red())
                     await ctx.send(embed=embed)
                     return
 
-        if DATABASE_EXTENSION is not ".json":
-            embed = discord.Embed(title="WARNING",
-                                  description=f"**Invalid database extension set!**\nIt looks like this value was changed.\n\n**Error:** Database must use .json files!\n*Revert this change, and then reload the module.*",
-                                  color=discord.Color.red())
-            await ctx.send(embed=embed)
-            return
-
-        def check(m):
-            return m.author == ctx.author
-
-        found = False
-        inv_accepted = True
-        channel = ""
-        username = ctx.message.author.name + "'s Projects"
-        category = get(ctx.guild.categories, name=username)
-        if category is None:
-            embed = discord.Embed(title="Whoops!",
-                                  description="You need to have an active project before inviting someone!",
-                                  color=discord.Color.red())
-            await ctx.send(embed=embed)
-            return
-        if len(category.channels) == 1:
-            for scan in category.channels:
-                channel = scan
-        if len(category.channels) > 1:
-            embed = discord.Embed(title="Select project",
-                                  description=f"**You have multiple projects open!**\nPlease specify which project you want to use\n\n*Reply to this message with the project name*",
-                                  color=discord.Color.blue())
-            await ctx.send(embed=embed)
-            msg = await self.bot.wait_for('message', timeout=3600.0, check=check)
-            for scan in category.channels:
-                if scan.name == msg.content:
-                    found = True
-                    channel = scan
-            if found is False:
-                embed = discord.Embed(title="Whoops!", description="You don't have a project with that name!",
-                                      color=discord.Color.red())
-                await ctx.send(embed=embed)
-                return
-
         if ALREADY_JOINED_CHECK is True:
-            for scan in channel.members:
+            for scan in use_channel.members:
                 if scan.id == member.id:
                     embed = discord.Embed(title="Whoops!", description=f"{member.name} is already in your project!", color=discord.Color.red())
                     await ctx.send(embed=embed)
                     return
 
+        inv_accepted = True
+
         if INVITE_SYSTEM is True:
-            embed = discord.Embed(title="Exciting news!",
-                                  description=f"**You've been invited!**\nBelow you'll find the project details\n\n**Project:** {scan.name.capitalize()}\n**Owner:** {ctx.author.name}\n**Members:** {len(scan.members)}\n**Description:** {scan.topic}\n\n*You have 1 hour to accept this invite*",
-                                  color=discord.Color.blue())
-            msg = await member.send(embed=embed)
+            embed = discord.Embed(title="Exciting news!", description=f"**You've been invited!**\nBelow you'll find the project details\n\n**Owner:** {ctx.author.name}\n**Members:** {members}\n**Description:**\n{description}\n\n*You have 1 hour to accept this invite*", color=discord.Color.blue())
+            msg = await member.send(embed=embed)  
             await msg.add_reaction('\N{THUMBS UP SIGN}')
             await msg.add_reaction('\N{THUMBS DOWN SIGN}')
-
             def dmloc(reaction, user):
                 return user.id == member.id
-
-            embed = discord.Embed(title="Consider it done!",
-                                  description=f"**Invite was sent to {member.name} successfully!**\n*They have 1 hour to accept it*",
-                                  color=discord.Color.blue())
+            embed = discord.Embed(title="Consider it done!", description=f"**Invite was sent to {member.name} successfully!**\n*They have 1 hour to accept it*", color=discord.Color.blue())
             await ctx.send(embed=embed)
-            response, username = await self.bot.wait_for('reaction_add', timeout=60.0, check=dmloc)
+            response, username = await self.bot.wait_for('reaction_add', timeout=3600.0, check=dmloc)
             if response.emoji == '\N{THUMBS UP SIGN}':
-                embed = discord.Embed(title=f"{member.name} accepted your invite!",
-                                      description=f"Welcome to the project {member.mention}!",
-                                      color=discord.Color.blue())
-                await channel.send(embed=embed)
+                embed = discord.Embed(title=f"{member.name} accepted your invite!", description=f"Go welcome them to your project!", color=discord.Color.blue())
+                await ctx.author.send(embed=embed)
+                embed = discord.Embed(title=f"{member.name} joined!", color=discord.Color.blue())
+                await use_channel.send(embed=embed)
             else:
-                embed = discord.Embed(title=f"{member.name} rejected your invite!",
-                                      description="Talk with users before inviting them!", color=discord.Color.red())
-                await channel.send(embed=embed)
+                embed = discord.Embed(title=f"{member.name} rejected your invite!", description="Talk with users before inviting them!", color=discord.Color.red())
+                await ctx.author.send(embed=embed)
                 inv_accepted = False
         if inv_accepted is True:
             overwrite = discord.PermissionOverwrite()
             overwrite.send_messages = WRITE_PERMISSION
             overwrite.read_messages = True
             overwrite.read_message_history = True
-            await channel.set_permissions(member, overwrite=overwrite)
+            for setperm in perms:
+                await setperm.set_permissions(member, overwrite=overwrite)
             if INVITE_SYSTEM is False:
-                embed = discord.Embed(title=f"{member.name} was added!",
-                                      description=f"Welcome to the project {member.mention}!\n\n*Since the invite system is currently disabled,\nyou were automatically added.*",
-                                      color=discord.Color.blue())
-                await channel.send(embed=embed)
+                embed = discord.Embed(title=f"{member.name} was added!", description=f"Welcome to the project {member.mention}!\n\n*Since the invite system is currently disabled,\nyou were automatically added.*", color=discord.Color.blue())
+                await use_channel.send(embed=embed)
 
     @commands.has_role("Verified Developer")
     @commands.command()
